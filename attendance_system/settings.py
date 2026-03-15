9b86b80a5a978453b5111a2fcd66c487bb88656f
woyi17 <woyi171717@gmail.com>
2026-03-15 19:16:19 +0000

    Initial commit

--- attendance_system/settings.py
+++ attendance_system/settings.py
@@ -49,11 +49,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'attendance_system.wsgi.application'
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
